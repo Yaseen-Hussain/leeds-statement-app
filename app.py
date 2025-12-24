c9a32fa1ff186ff8a070e975fcb72cffff6c5a25
Yaseen <yaseenh0@gmail.com>
2025-12-24 16:04:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from google.oauth2.service_account import Credentials
 from io import BytesIO
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
+from jinja2 import Template
+
 
 
 # ---------------- CONFIG ----------------
